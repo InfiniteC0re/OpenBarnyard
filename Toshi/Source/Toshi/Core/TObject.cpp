41f15cdefd69cc7355a6e87e43cf5af3ead2bbce
InfiniteC0re <inf.core0@gmail.com>
2022-08-19 11:16:45 +0500

    fixed formatting in TObject.cpp

--- Toshi/Source/Toshi/Core/TObject.cpp
+++ Toshi/Source/Toshi/Core/TObject.cpp
@@ -3,30 +3,30 @@
 
 namespace Toshi
 {
-	// static initialization of TObject's TClass
-	constinit TClassProps TObject::s_Class
-	{
-		"TObject",								// m_Name
-		TObject::CreateTObject,					// m_Create
-		TObject::CreateTObjectInPlace,			// m_CreateInPlace
-		nullptr,								// m_Initialize
-		nullptr,								// m_Uninitialize
-		nullptr,								// m_Parent
-		nullptr,								// m_Previous
-		nullptr,								// m_LastAttached,
-		MKVERSION(1, 0),						// m_Version
-		sizeof(TObject),						// m_Size
-		0,										// m_Unk
-		false									// m_Initialized
-	};
+    // static initialization of TObject's TClass
+    constinit TClassProps TObject::s_Class
+    {
+        "TObject",                              // m_Name
+        TObject::CreateTObject,                 // m_Create
+        TObject::CreateTObjectInPlace,          // m_CreateInPlace
+        nullptr,                                // m_Initialize
+        nullptr,                                // m_Uninitialize
+        nullptr,                                // m_Parent
+        nullptr,                                // m_Previous
+        nullptr,                                // m_LastAttached,
+        MKVERSION(1, 0),                        // m_Version
+        sizeof(TObject),                        // m_Size
+        0,                                      // m_Unk
+        false                                   // m_Initialized
+    };
 
-	TObjectInterface::~TObjectInterface()
-	{
-		TOSHI_CORE_INFO("Called ~TObject");
-	}
+    TObjectInterface::~TObjectInterface()
+    {
+        TOSHI_CORE_INFO("Called ~TObject");
+    }
 
-	void TObjectInterface::Destroy()
-	{
-		delete this;
-	}
+    void TObjectInterface::Destroy()
+    {
+        delete this;
+    }
 }
