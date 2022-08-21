803a0cd35db98866bb2cfe0240b14e3d6d76931e
LeonNepelius <leon@nepelius.at>
2022-08-21 18:16:46 +0200

    Work on TNodes

--- Toshi/Source/Toshi/Core/TGenericDList.cpp
+++ Toshi/Source/Toshi/Core/TGenericDList.cpp
@@ -3,5 +3,46 @@
 
 Toshi::TGenericDList::TNode::TNode()
 {
+	Reset();
+}
 
+void Toshi::TGenericDList::TNode::InsertAfter(TNode* param_1)
+{
+	m_pPrevNode = param_1;
+	m_pNextNode = param_1->m_pNextNode;
+	param_1->m_pNextNode = this;
+	m_pNextNode->m_pPrevNode = this;
+}
+
+void Toshi::TGenericDList::TNode::InsertBefore(TNode* param_1)
+{
+	m_pNextNode = param_1;
+	m_pPrevNode = param_1->m_pPrevNode;
+	param_1->m_pPrevNode = this;
+	m_pPrevNode->m_pNextNode = this;
+}
+
+void Toshi::TGenericDList::TNode::Remove()
+{
+	m_pPrevNode->m_pNextNode = m_pNextNode;
+	m_pNextNode->m_pPrevNode = m_pPrevNode;
+	m_pNextNode = this;
+	m_pPrevNode = this;
+}
+
+void Toshi::TGenericDList::TNode::Reset()
+{
+	m_pNextNode = this;
+	m_pPrevNode = this;
+}
+
+Toshi::TGenericDList::TNode& Toshi::TGenericDList::TNode::operator=(const TNode& param_1)
+{
+	m_pNextNode = param_1.m_pNextNode;
+	m_pPrevNode = param_1.m_pPrevNode;
+	return *this;
+}
+
+Toshi::TGenericDList::TGenericDList()
+{
 }
--- Toshi/Source/Toshi/Core/TGenericDList.h
+++ Toshi/Source/Toshi/Core/TGenericDList.h
@@ -1,18 +1,30 @@
 #pragma once
 namespace Toshi
 {
+	//template <typename T, int>
 	class TGenericDList
 	{
+		TGenericDList();
 
 		class TNode
 		{
 		protected:
 			TNode();
+			// TNode(TUninitialized param1);
 
 			inline TNode* Next() const { return m_pNextNode; }
+			inline TNode* Prev() const { return m_pPrevNode; }
+			inline bool IsLinked() const { return this != m_pNextNode; }
 
-			TNode* m_pNextNode;
-			TNode* m_pPrevNode;
+			void InsertAfter(TNode*);
+			void InsertBefore(TNode*);
+			void Remove();
+			void Reset();
+
+			TNode& operator=(const TNode& param_1);
+
+			TNode* m_pNextNode; // 0x0
+			TNode* m_pPrevNode; // 0x4
 		};
 	};
 
