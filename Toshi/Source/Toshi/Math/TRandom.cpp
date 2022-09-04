2ccdbd35022f5aced15d5c8caa3a5c1bac3381c5
InfiniteC0re <inf.core0@gmail.com>
2022-09-04 15:49:10 +0500

    moved macros from TRandom.h to TRandom.cpp

--- Toshi/Source/Toshi/Math/TRandom.cpp
+++ Toshi/Source/Toshi/Math/TRandom.cpp
@@ -1,6 +1,40 @@
 #include "ToshiPCH.h"
 #include "TRandom.h"
 
+#ifdef USE_PORTABLE
+#define cut(a)     ((a) & 0xffffffff) /* Cut the integer down to 32bits */
+#else
+#define cut(a)     (a) /* A no-op */
+#endif
+
+#define ind(mm,x)  ((mm)[(x>>2)&(RANDSIZ-1)])
+/* the call to cut() is a macro defined in standard.h */
+#define rngstep(mix,a,b,mm,m,m2,r,x) \
+{ \
+  x = *m;  \
+  a = cut((a^(mix)) + *(m2++)); \
+  *(m++) = y = cut(ind(mm,x) + a + b); \
+  *(r++) = b = cut(ind(mm,y>>RANDSIZL) + x); \
+}
+#define mix(a,b,c,d,e,f,g,h) \
+{ \
+  a^=b<<11; d+=a; b+=c; \
+  b^=c>>2;  e+=b; c+=d; \
+  c^=d<<8;  f+=c; d+=e; \
+  d^=e>>16; g+=d; e+=f; \
+  e^=f<<10; h+=e; f+=g; \
+  f^=g>>4;  a+=f; g+=h; \
+  g^=h<<8;  b+=g; h+=a; \
+  h^=a>>9;  c+=h; a+=b; \
+}
+#define shuffle(a, b, mm, m, m2, r, x) \
+{ \
+  rngstep(a<<13, a, b, mm, m, m2, r, x); \
+  rngstep(a>>6 , a, b, mm, m, m2, r, x); \
+  rngstep(a<<2 , a, b, mm, m, m2, r, x); \
+  rngstep(a>>16, a, b, mm, m, m2, r, x); \
+}
+
 namespace Toshi
 {
 	TRandom::TRandom()
--- Toshi/Source/Toshi/Math/TRandom.h
+++ Toshi/Source/Toshi/Math/TRandom.h
@@ -11,40 +11,6 @@ MODIFIED:
 */
 // mix with source: https://github.com/jawnsy/Math-Random-ISAAC-XS/blob/master/src/rand.c
 
-#ifdef USE_PORTABLE
-#define cut(a)     ((a) & 0xffffffff) /* Cut the integer down to 32bits */
-#else
-#define cut(a)     (a) /* A no-op */
-#endif
-
-#define ind(mm,x)  ((mm)[(x>>2)&(RANDSIZ-1)])
-/* the call to cut() is a macro defined in standard.h */
-#define rngstep(mix,a,b,mm,m,m2,r,x) \
-{ \
-  x = *m;  \
-  a = cut((a^(mix)) + *(m2++)); \
-  *(m++) = y = cut(ind(mm,x) + a + b); \
-  *(r++) = b = cut(ind(mm,y>>RANDSIZL) + x); \
-}
-#define mix(a,b,c,d,e,f,g,h) \
-{ \
-  a^=b<<11; d+=a; b+=c; \
-  b^=c>>2;  e+=b; c+=d; \
-  c^=d<<8;  f+=c; d+=e; \
-  d^=e>>16; g+=d; e+=f; \
-  e^=f<<10; h+=e; f+=g; \
-  f^=g>>4;  a+=f; g+=h; \
-  g^=h<<8;  b+=g; h+=a; \
-  h^=a>>9;  c+=h; a+=b; \
-}
-#define shuffle(a, b, mm, m, m2, r, x) \
-{ \
-  rngstep(a<<13, a, b, mm, m, m2, r, x); \
-  rngstep(a>>6 , a, b, mm, m, m2, r, x); \
-  rngstep(a<<2 , a, b, mm, m, m2, r, x); \
-  rngstep(a>>16, a, b, mm, m, m2, r, x); \
-}
-
 #define RANDSIZL  (4)  /* 8 for crypto, 4 for simulations (aka Toshi uses 4) */
 #define RANDSIZ   (1 << RANDSIZL)
 
