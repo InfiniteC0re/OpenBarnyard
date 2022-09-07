f7c757222b38c6209ba1b13c91d12d289519c21a
InfiniteC0re <inf.core0@gmail.com>
2022-09-07 15:16:56 +0500

    TTSF

--- Toshi/Source/Toshi/File/TTRB.h
+++ Toshi/Source/Toshi/File/TTRB.h
@@ -8,24 +8,13 @@ namespace Toshi
 
 	class TTRB
 	{
-		enum class Endianess
+	public:
+		enum TTRB_ERROR
 		{
-			Little,
-			Big,
-		};
-
-		enum class Error
-		{
-			Success = 0,
-			WrongMagic = 4,
-			NoFile = 6,
-			NotTRB = 7,
-		};
-
-		struct Header
-		{
-			uint32_t m_Magic;
-			uint32_t m_FileSize;
+			ERROR_OK = 0,
+			ERROR_WRONG_MAGIC = 4,
+			ERROR_NO_FILE = 6,
+			ERROR_NOT_TRB = 7,
 		};
 
 		struct SecInfo // Size 0x10
@@ -33,36 +22,18 @@ namespace Toshi
 
 		};
 
-		// idk how to call it and what's it's purpose
-		struct FileInfo
+		struct Header
 		{
-			uint32_t FileStartOffset; // offset to TRBF
-			uint32_t FileSize;        // just the size
+			uint32_t m_ui32Version; // 0x0
+			uint32_t m_i32SectionCount; // 0x4
+			SecInfo* m_pSecInfo; // Name not confirmed
 		};
-
-		//struct Header
-		//{
-		//	uint32_t m_ui32Version; // 0x0
-		//	uint32_t m_i32SectionCount; // 0x4
-		//	SecInfo* m_pSecInfo; // Name not confirmed
-		//};
 
 	public:
 		bool LoadTrb(const char*);
-		Error ReadFile(TFile* a_pFile);
-		Error SaveFileInfo();
 
 	private:
-		// Header* m_pHeader;          // 0x0 (it's not from TTRB)
-		Endianess m_iEndianess;     // 0x0
-		TFile* m_pFile;             // 0x4
-		uint32_t m_FileInfoCount;   // 0x8
-		FileInfo m_FileInfo[32];    // 0xC
-		Header m_Header;            // 0x10C
-		uint32_t m_TRBF;            // 0x114
-		uint32_t m_FileMagic;       // 0x118
-		uint32_t m_FileSize;        // 0x11C
-		uint32_t m_Unk;             // 0x120
+		Header* m_pHeader;          // 0x0 (it's not from TTRB)
 	};
 }
 
--- Toshi/Source/Toshi/File/TTSF.cpp
+++ Toshi/Source/Toshi/File/TTSF.cpp
@@ -1,2 +1,65 @@
 #include "ToshiPCH.h"
 #include "TTSF.h"
+#include "TTRB.h"
+
+namespace Toshi
+{
+	uint8_t TTSF::ReadFile(TFile* a_pFile)
+	{
+		// FUN_00686920
+
+		m_pFile = a_pFile;
+		m_FileInfoCount = 0;
+
+		if (m_pFile == TNULL)
+		{
+			return TTRB::ERROR_NO_FILE;
+		}
+
+		m_pFile->Read(&m_Header, sizeof(TTSF::Header));
+
+		if (m_Header.m_Magic == TMAKEFOUR("TSFL"))
+		{
+			m_iEndianess = Endianess::Little;
+		}
+		else
+		{
+			if (m_Header.m_Magic != TMAKEFOUR("TSFB"))
+			{
+				return TTRB::ERROR_NOT_TRB;
+			}
+
+			m_iEndianess = Endianess::Big;
+		}
+
+		m_ReadPos += m_pFile->Read(&m_TRBF, 4);
+
+		if (m_iEndianess == Endianess::Big)
+		{
+			m_Header.m_FileSize = BIG_ENDIAN_TO_LITTLE_UINT32(m_Header.m_FileSize);
+			m_TRBF = BIG_ENDIAN_TO_LITTLE_UINT32(m_TRBF);
+		}
+
+		m_FileMagic = m_Header.m_Magic;
+		m_FileSize = m_Header.m_FileSize;
+		SaveFileInfo();
+
+		return TTRB::ERROR_OK;
+	}
+
+	uint8_t TTSF::SaveFileInfo()
+	{
+		// FUN_00686130
+
+		if (m_FileMagic != TMAKEFOUR("FORM") && m_FileMagic != TMAKEFOUR("TSFL") && m_FileMagic != TMAKEFOUR("TSFB"))
+		{
+			return TTRB::ERROR_WRONG_MAGIC;
+		}
+
+		m_FileInfo[m_FileInfoCount].FileStartOffset = m_pFile->Tell() - 4;
+		m_FileInfo[m_FileInfoCount].FileSize = m_FileSize;
+		m_FileInfoCount++;
+
+		return TTRB::ERROR_OK;
+	}
+}
