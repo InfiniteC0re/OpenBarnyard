c2417cddef23fc9ec4d296f386b4d80c31ccb42c
LeonNepelius <leon@nepelius.at>
2022-09-10 16:57:46 +0200

    Some LoadTMD work

--- Toshi/Source/Toshi/Render/TModel.cpp
+++ Toshi/Source/Toshi/Render/TModel.cpp
@@ -1,2 +1,20 @@
 #include "ToshiPCH.h"
 #include "TModel.h"
+
+bool Toshi::TModel::LoadTMD(const char* a_tmdPath)
+{
+	auto fs = Toshi::TFileSystem::CreateNative("local");
+	auto file = fs->CreateFile(a_tmdPath, Toshi::TFile::OpenFlags_Read);
+
+	Header header;
+
+	if (file != TNULL)
+	{
+		file->Read(&header, sizeof(Header));
+		if (header.m_magic == TMAKEFOUR("TMDL"))
+		{
+
+		}
+	}
+	return false;
+}
--- Toshi/Source/Toshi/Render/TModel.h
+++ Toshi/Source/Toshi/Render/TModel.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include "Toshi/File/TFile.h"
+
 namespace Toshi
 {
 	class TModel
@@ -7,9 +10,24 @@ namespace Toshi
 		int m_iLODCount; // 0xC
 		void* m_pSkeleton; // 0x14
 
+		struct Header
+		{
+			uint32_t m_magic;
+			uint32_t m_version;
+			uint32_t m_remainingBytes;
+		};
+
+		struct Header2
+		{
+
+		};
+
 		inline bool IsCreated() { return (m_Flags & 1) != 0; }
 
 		inline int GetLODCount() { return m_iLODCount; }
+
+	protected:
+		bool LoadTMD(const char*); // JPOG only i think not sure though
 	};
 }
 
