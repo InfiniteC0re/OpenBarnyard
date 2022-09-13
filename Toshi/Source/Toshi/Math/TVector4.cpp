fcf33949360f3a2361594b75b2d0dd228c6abe43
LeonNepelius <leon@nepelius.at>
2022-09-13 17:00:56 +0200

    TVector4 work done

--- Toshi/Source/Toshi/Math/TVector4.cpp
+++ Toshi/Source/Toshi/Math/TVector4.cpp
@@ -3,6 +3,53 @@
 
 namespace Toshi
 {
+	void TVector4::Abs(const TVector4& a_rVec4)
+	{
+		x = TMath::Abs(a_rVec4.x);
+		y = TMath::Abs(a_rVec4.y);
+		z = TMath::Abs(a_rVec4.z);
+		w = TMath::Abs(a_rVec4.w);
+	}
+
+	void TVector4::Abs()
+	{
+		x = TMath::Abs(x);
+		y = TMath::Abs(y);
+		z = TMath::Abs(z);
+		w = TMath::Abs(w);
+	}
+
+	void TVector4::Add(const TVector4& a_rVec4, const TVector4& a_rVec4_2)
+	{
+		x = a_rVec4.x + a_rVec4_2.x;
+		y = a_rVec4.y + a_rVec4_2.y;
+		z = a_rVec4.z + a_rVec4_2.z;
+		y = a_rVec4.w + a_rVec4_2.w;
+	}
+
+	void TVector4::Add(const TVector4& a_rVec4)
+	{
+		x += a_rVec4.x;
+		y += a_rVec4.y;
+		z += a_rVec4.z;
+	}
+
+	void TVector4::Cos(const TVector4& a_rVec4)
+	{
+		x = TMath::ACos(a_rVec4.x);
+		y = TMath::ACos(a_rVec4.y);
+		z = TMath::ACos(a_rVec4.z);
+		w = TMath::ACos(a_rVec4.w);
+	}
+
+	void TVector4::CrossProduct(const TVector4& a_rVec4, const TVector4& a_rVec4_2)
+	{
+		TMathFloating fX = a_rVec4_2.z * a_rVec4.y - a_rVec4_2.y * a_rVec4.z;
+		TMathFloating fY = a_rVec4.z * a_rVec4_2.x - a_rVec4_2.z * a_rVec4.x;
+		TMathFloating fZ = a_rVec4.x * a_rVec4_2.y - a_rVec4.y * a_rVec4_2.x;
+		Set(fX, fY, fZ, w);
+	}
+
 	void TVector4::Negate3()
 	{
 		x = -x;
@@ -16,5 +63,21 @@ namespace Toshi
 		y = a_rVec3.y;
 		z = a_rVec3.z;
 		w = 1.0f;
+	}
+
+	void TVector4::Set(float a_fVal1, float a_fVal2, float a_fVal3, float a_fVal4)
+	{
+		x = a_fVal1;
+		y = a_fVal2;
+		z = a_fVal3;
+		w = a_fVal4;
+	}
+
+	void TVector4::Set(float a_pfVal[4])
+	{
+		x = a_pfVal[0];
+		y = a_pfVal[1];
+		z = a_pfVal[2];
+		w = a_pfVal[3];
 	}
 }
--- Toshi/Source/Toshi/Math/TVector4.h
+++ Toshi/Source/Toshi/Math/TVector4.h
@@ -17,8 +17,18 @@ namespace Toshi
 	class TVector4 : public Props::Vec4
 	{
 	public:
+		void Abs(const TVector4& a_rVec4);
+		void Abs();
+		void Add(const TVector4& a_rVec4, const TVector4& a_rVec4_2);
+		void Add(const TVector4& a_rVec4);
+		inline const TVector3& AsVector3() const { return reinterpret_cast<const TVector3&>(*this); }
+		inline TVector3& AsVector3() { return reinterpret_cast<TVector3&>(*this); }
+		void Cos(const TVector4& a_rVec4);
+		void CrossProduct(const TVector4& a_rVec4, const TVector4& a_rVec4_2);
 		void Negate3();
 		void Set(const TVector3& a_rVec3);
+		void Set(float a_fVal1, float a_fVal2, float a_fVal3, float a_fVal4);
+		void Set(float a_pfVal[4]);
 	};
 
 	static_assert(sizeof(TVector4) == sizeof(Props::Vec4));
