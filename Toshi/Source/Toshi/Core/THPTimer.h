37a2de2340f995159a86a2ab40a609f4fd553981
InfiniteC0re <inf.core0@gmail.com>
2022-12-08 01:32:50 +0500

    separate THPTimer for multiplatform

--- Toshi/Source/Toshi/Core/THPTimer.h
+++ Toshi/Source/Toshi/Core/THPTimer.h
@@ -1,28 +1,7 @@
 #pragma once
 
-namespace Toshi
-{
-	class THPTimer
-	{
-	public:
-		THPTimer();
-
-		uint32_t GetRaw32();
-		int64_t GetRaw64();
-
-		void Reset();
-		void Update();
-
-	public:
-		inline float GetDelta() const { return m_fDelta; }
-		inline float GetCurrentSeconds() const { return m_fCurrentSeconds; }
-
-	private:
-		LARGE_INTEGER m_iFrequency;        // 0x0
-		float m_fCurrentSeconds;           // 0x8
-		LARGE_INTEGER m_iOldTime;          // 0x10
-		LARGE_INTEGER m_iCurrentTime;      // 0x18
-		float m_fDelta;                    // 0x20
-	};
-}
-
+#ifdef TOSHI_PLATFORM_WINDOWS
+#include "Platform/Windows/THPTimer_Win.h"
+#else
+#error "Unsupported platform"
+#endif
