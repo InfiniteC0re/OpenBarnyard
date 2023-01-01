0bdcbdb8d701004b6158c176c622394e617b9811
AdventureT <le@nepelius.at>
2023-01-01 21:23:04 +0100

    Some XUI changes

--- Toshi/Source/Toshi/Xui/TXUIElement.cpp
+++ Toshi/Source/Toshi/Xui/TXUIElement.cpp
@@ -23,8 +23,8 @@ bool Toshi::XURXUIElementData::Load(uint8_t*& a_pData)
 		int smth2 = 0;
 		if (unk != 0)
 		{
-			smth2 = PARSEWORD_BIG(a_pData);
-			a_pData += 2;
+			smth2 = PARSEWORD_BIG(a_pData + 1);
+			a_pData += 3;
 		}
 		if ((smth2 & 1) != 0)
 		{
@@ -33,12 +33,14 @@ bool Toshi::XURXUIElementData::Load(uint8_t*& a_pData)
 		}
 		if ((smth2 & 2) != 0)
 		{
-			m_unk2 = PARSEDWORD_BIG(a_pData);
+			int test = PARSEDWORD_BIG(a_pData);
+			m_width = static_cast<float>(PARSEDWORD_BIG(a_pData));
 			a_pData += 4;
 		}
 		if ((smth2 & 4) != 0)
 		{
-			m_unk2 = PARSEDWORD_BIG(a_pData);
+			int test = PARSEDWORD_BIG(a_pData);
+			m_height = static_cast<float>(PARSEDWORD_BIG(a_pData));
 			a_pData += 4;
 		}
 		if ((smth2 & 8) != 0)
@@ -121,13 +123,71 @@ bool Toshi::XURXUIElementData::TranslateTimelineProp(const char* param_1, uint32
 		param_3 = 0;
 		return true;
 	}
-	else
+	else if (TStringManager::String8Compare(param_1, "Width", -1) == 0)
 	{
-		if (TStringManager::String8Compare(param_1, "Width", -1) == 0)
-		{
-			param_3 = 1;
-			return true;
-		}
+		param_3 = 1;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Height", -1) == 0)
+	{
+		param_3 = 2;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Position", -1) == 0)
+	{
+		param_3 = 3;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Scale", -1) == 0)
+	{
+		param_3 = 4;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Rotation", -1) == 0)
+	{
+		param_3 = 5;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Opacity", -1) == 0)
+	{
+		param_3 = 6;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Anchor", -1) == 0)
+	{
+		param_3 = 7;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Pivot", -1) == 0)
+	{
+		param_3 = 8;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "Show", -1) == 0)
+	{
+		param_3 = 9;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "BlendMode", -1) == 0)
+	{
+		param_3 = 10;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "DisableTimelineRecursion", -1) == 0)
+	{
+		param_3 = 11;
+		return true;
+	}
+	// This is correct for some reason they skipped 12
+	else if (TStringManager::String8Compare(param_1, "ColorWriteFlags", -1) == 0)
+	{
+		param_3 = 13;
+		return true;
+	}
+	else if (TStringManager::String8Compare(param_1, "ClipChidren", -1) == 0)
+	{
+		param_3 = 14;
+		return true;
 	}
 	return false;
 }
--- Toshi/Source/Toshi/Xui/TXUIElement.h
+++ Toshi/Source/Toshi/Xui/TXUIElement.h
@@ -44,22 +44,39 @@ namespace Toshi
 	public:
 		XURXUIElementData() : XURXUIObjectData()
 		{
-
+			m_width = 60.0f;
+			m_height = 30.0f;
+			m_positon = 1.0f;
+			m_scale = { -1.0f, -1.0f, -1.0f };
+			m_rotation = { -1.0f, 0.0f, 0.0f };
+			m_opacity = 0;
+			m_pivot = 0;
+			m_show = 0;
+			m_disableTimelineRecursion = true;
+			m_clipChildren = true;
 		}
 
-		uint16_t m_unk2; //0x1C de blob
-		uint16_t m_unk3; //0x1E de blob
-		uint16_t m_unk4; //0x20 de blob
-		uint16_t m_unk5; //0x22 de blob
-		uint16_t m_unk6; //0x24 de blob
-		uint16_t m_unk7; //0x26 de blob
-		uint16_t m_unk; //0x28 de blob
-		uint32_t m_unkFlags; //0x2A de blob
-		uint32_t m_fOpacity; //0x2C de blob
+		~XURXUIElementData() = default;
+
+		float m_width; //0x24
+		float m_height; //0x28
+		float m_positon; //0x2C
+		TVector3 m_scale; //0x30
+		TVector3 m_rotation;
+		uint16_t m_opacity;
+		uint16_t m_anchor; 
+		uint32_t m_pivot; 
+		uint32_t m_show; 
+		uint32_t m_disableTimelineRecursion;
+		bool m_colorWriteFlags;
+		bool m_clipChildren;
+
 
 		bool Load(uint8_t*& a_pData);
 
 		bool TranslateTimelineProp(const char* param_1, uint32_t& param_2, uint32_t& param_3);
+
+		bool ValidateTimelineProp(uint32_t unused, uint32_t param_2) { return true; }
 
 		bool IsFloatPropType(uint32_t unused, uint32_t propType) {
 			return propType != 1 && propType != 2 && propType != 6 ? false : true;
