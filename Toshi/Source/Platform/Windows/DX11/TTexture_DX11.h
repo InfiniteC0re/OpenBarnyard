69638ecdc37d5d1885ead47d042fe8cb585646d6
InfiniteC0re <inf.core0@gmail.com>
2023-01-28 21:22:50 +0500

    little work on TTexture

--- Toshi/Source/Platform/Windows/DX11/TTexture_DX11.h
+++ Toshi/Source/Platform/Windows/DX11/TTexture_DX11.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Toshi/Strings/TStringManager.h"
 #include "Toshi/File/TFile.h"
 
@@ -7,14 +6,29 @@ namespace Toshi
 {
 	class TTexture
 	{
-		char* m_unk; //0x0
-		char* m_imageName; //0x4
-		char* m_unk2; //0x8
-
-		uint32_t m_DataSize; // 0xC
-		uint8_t* m_TexData; // 0x10
+	public:
+		struct Info
+		{
+			UINT Width;
+			UINT Height;
+			UINT MipMapCount;
+			UINT16 Width2;
+			UINT16 Height2;
+			DXGI_FORMAT Format;
+			ID3D11ShaderResourceView* pSRView;
+		};
 
 	public:
 		void Init();
+
+		// static TTexture* CreateTexture(DXGI_FORMAT format, UINT width, UINT height, const void* srcData);
+
+	private:
+		void* m_Unk;              // 0x00
+		char* m_TexName;          // 0x04
+		Info* m_TexInfo;          // 0x08
+		uint32_t m_DataSize;      // 0x0C
+		uint8_t* m_TexData;       // 0x10
+		TTexture* m_pNextTexture; // 0x14
 	};
 }
