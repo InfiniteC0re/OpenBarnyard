0a0d21d0fe05ce996efcbed9e0f81e2e584ada10
AdventureT <le@nepelius.at>
2023-02-10 17:16:11 +0100

    Some TEvent impl

--- Toshi/Source/Toshi/Core/TEvent.h
+++ Toshi/Source/Toshi/Core/TEvent.h
@@ -4,6 +4,20 @@
 
 namespace Toshi
 {
+
+	class TGenericListener
+	{
+	public:
+
+		using t_Func = bool (*)(void*, void*, void*);
+		void* m_pUnk;
+		void* m_pCaller;
+		t_Func* m_pCallback;
+
+	protected:
+		//void Connect(TGenericEmitter* emitter, void* unk1, t_Func func, int unk2);
+	};
+
 	template <class T>
 	class TGenericEmitter : public TPriList<T>::TNode
 	{
@@ -11,25 +25,26 @@ namespace Toshi
 		TGenericEmitter() = default;
 		TGenericEmitter(void* owner) 
 		{
-			m_List.m_Root.m_Next = this;
-			m_List.m_Root.m_Prev = this;
+			m_Listeners.m_Root.m_Next = this;
+			m_Listeners.m_Root.m_Prev = this;
 			Create(owner); 
 		}
 
 		TGenericEmitter(const TGenericEmitter& cpy)
 		{
-			m_List.m_Root.m_Next = cpy.m_List.m_Root.m_Next;
-			m_List.m_Root.m_Prev = cpy.m_List.m_Root.m_Prev;
+			m_Listeners.m_Root.m_Next = cpy.m_Listeners.m_Root.m_Next;
+			m_Listeners.m_Root.m_Prev = cpy.m_Listeners.m_Root.m_Prev;
 			Create(cpy.m_Owner);
 		}
 
 		void Throw(void* owner)
 		{
-			auto pTVar2 = m_List.m_Root.m_Next;
+			auto curListener = m_Listeners.m_Root.m_Next->As<TGenericListener>();
 			do
 			{
-				if (pTVar2 == this) return;
-				pTVar2 = m_List.m_Root.m_Next;
+				if (curListener == this) return;
+				curListener->m_pCallback(curListener->m_pCaller, m_Owner, owner);
+				curListener = m_Listeners.m_Root.m_Next->As<TGenericListener>();
 			} while (true);
 		}
 		void Create(void* owner) { m_Owner = owner; }
@@ -41,17 +56,9 @@ namespace Toshi
 		}
 
 	private:
-		TPriList<T> m_List;
+		TPriList<TGenericListener> m_Listeners;
 		void* m_Owner;
 	};
 
-	//class TGenericListener
-	//{
-	//public:
 
-	//	using t_Func = bool (*)(void*, void*, void*);
-
-	//protected:
-	//	//void Connect(TGenericEmitter* emitter, void* unk1, t_Func func, int unk2);
-	//};
 }
