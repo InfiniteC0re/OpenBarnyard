7415422e657c9cbb8e26e808888cf7ac0f43c0be
AdventureT <le@nepelius.at>
2023-06-12 18:30:05 +0200

    A bit ACamera work

---
+++ OpenBlob/Source/Cameras/ACamera.cpp
@@ -0,0 +1,5 @@
+#include "pch.h"
+#include "ACamera.h"
+
+const Toshi::TVector4 ACamera::sm_vInitialPos = { 1339.0f, -10.0f, 1495.0f, 1.0f };
+const Toshi::TVector4 ACamera::sm_vWorldUp = { 0.0f, -1.0f, 0.0f, 1.0f };
---
+++ OpenBlob/Source/Cameras/ACamera.h
@@ -0,0 +1,47 @@
+#pragma once
+
+class ACamera :
+	public Toshi::TGenericClassDerived<ACamera, Toshi::TObject, "ACamera", TMAKEVERSION(1, 0), false>
+{
+
+	static constexpr float sm_fDefaultFOV = 1.0471976f;
+	static const Toshi::TVector4 sm_vInitialPos;
+	static const Toshi::TVector4 sm_vWorldUp;
+
+	enum CameraType
+	{
+		CT_TYPECOUNT = 4
+	};
+
+	ACamera(CameraType a_eCamType) : m_eCamType(a_eCamType)
+	{
+		InitMatrices(m_mMatrices);
+		TASSERT(a_eCamType >= 0 && a_eCamType < CT_TYPECOUNT);
+	}
+
+	static void InitMatrices(Toshi::TMatrix44* matrices)
+	{
+		matrices[0].Identity();
+		matrices[1].Identity();
+		TTODO("...");
+	}
+
+	void SetMatrix(const Toshi::TMatrix44& a_mMatrix)
+	{
+		m_mMatrices[0] = a_mMatrix;
+	}
+
+	void SetShakeMatrix(const Toshi::TMatrix44& a_mSkakeMatrix)
+	{
+		m_mMatrices[1] = a_mSkakeMatrix;
+	}
+
+	void LookAtDirection(const Toshi::TVector4& a_vDirection)
+	{
+		TIMPLEMENT();
+	}
+
+	CameraType m_eCamType;             // 0x8
+	Toshi::TMatrix44 m_mMatrices[2];   // 0x6C
+};
+
