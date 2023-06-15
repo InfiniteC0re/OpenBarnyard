f1c9bd5f7ec4cae0fb08769b59e425229a0b7ad7
InfiniteC0re <inf.core0@gmail.com>
2023-06-15 18:59:34 +0500

    Basic implementation of ACameraManager

--- OpenBlob/Source/Cameras/ACameraManager.cpp
+++ OpenBlob/Source/Cameras/ACameraManager.cpp
@@ -1,3 +1,22 @@
 #include "pch.h"
 #include "ACameraManager.h"
+#include "AFreeCamera.h"
 
+using namespace Toshi;
+
+ACameraManager::ACameraManager()
+{
+	TTODO("Some function calls");
+
+	for (size_t i = 0; i < MAX_CAMERA; i++)
+	{
+		m_ppCameras[i] = TNULL;
+	}
+
+	m_bFlag = TFALSE;
+	m_iCurrentCamera = -1;
+	m_iSomeCount = 0;
+
+	CreateCamera<AFreeCamera>();
+	TTODO("CreateCamera<AScriptedCamera>()");
+}
