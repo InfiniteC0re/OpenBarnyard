10f7e8b17cda3207a1f35aaa04105c6e728f4f5d
InfiniteC0re <inf.core0@gmail.com>
2023-06-17 16:50:56 +0500

    Use TOSHI_WARN for TFIXME

--- OpenBlob/Source/ImGui/imgui_tables.cpp
+++ OpenBlob/Source/ImGui/imgui_tables.cpp
@@ -184,10 +184,6 @@ Index of this file:
 // [SECTION] Header mess
 //-----------------------------------------------------------------------------
 
-#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
 #include "pch.h"
 #include "imgui.h"
 #ifndef IMGUI_DISABLE
