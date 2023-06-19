2c70de2e7cdcf358ee90445eb87ada6019214e91
InfiniteC0re <inf.core0@gmail.com>
2023-06-19 17:35:37 +0500

    Fix formatting in TTexture files

--- Toshi/Source/Platform/Windows/DX11/TTexture_DX11.cpp
+++ Toshi/Source/Platform/Windows/DX11/TTexture_DX11.cpp
@@ -8,145 +8,145 @@
 
 namespace Toshi
 {
-	void TTexture::Init()
-	{
-		TASSERT(0 < m_DataSize, "DataSize is not empty");
-		TRenderDX11* pRender = TRenderDX11::Interface();
-		TString8 path("Data\\TextureOverride\\");
-		path += m_TexName;
+    void TTexture::Init()
+    {
+        TASSERT(0 < m_DataSize, "DataSize is not empty");
+        TRenderDX11* pRender = TRenderDX11::Interface();
+        TString8 path("Data\\TextureOverride\\");
+        path += m_TexName;
 
-		ID3D11Resource* pResource = TNULL;
-		TFile* pFile = TFile::Create(path, TFile::FileMode_Read);
+        ID3D11Resource* pResource = TNULL;
+        TFile* pFile = TFile::Create(path, TFile::FileMode_Read);
 
-		if (pFile != TNULL)
-		{
-			if (path.Find(".tga") <= 0)
-			{
-				auto fileSize = pFile->GetSize();
-				uint8_t* mem = (uint8_t*)TMemalign(16, fileSize + 1);
-				pFile->Read(mem, fileSize);
-				mem[fileSize] = '\0';
+        if (pFile != TNULL)
+        {
+            if (path.Find(".tga") <= 0)
+            {
+                auto fileSize = pFile->GetSize();
+                uint8_t* mem = (uint8_t*)TMemalign(16, fileSize + 1);
+                pFile->Read(mem, fileSize);
+                mem[fileSize] = '\0';
 
-				DirectX::CreateDDSTextureFromMemory(pRender->m_pDevice, mem, fileSize, &pResource, &m_TexInfo->SRView, 0, TNULL);
+                DirectX::CreateDDSTextureFromMemory(pRender->m_pDevice, mem, fileSize, &pResource, &m_TexInfo->SRView, 0, TNULL);
 
-				TFree(mem);
-			}
-			else
-			{
-				TASSERT(path.IsIndexValid(0));
+                TFree(mem);
+            }
+            else
+            {
+                TASSERT(path.IsIndexValid(0));
 
-				int width, height, bpp;
+                int width, height, bpp;
 
-				stbi_uc* data = stbi_load(path, &width, &height, &bpp, 4);
+                stbi_uc* data = stbi_load(path, &width, &height, &bpp, 4);
 
-				if (data != NULL)
-				{
-					m_TexInfo->SRView = pRender->CreateTexture(width, height, DXGI_FORMAT_R8G8B8A8_UNORM, data, 0, D3D11_USAGE_IMMUTABLE, 0, 1);
-					if (m_TexInfo->SRView) pResource = TNULL;
-					stbi_image_free(data);
-				}
-				else
-				{
-					DirectX::CreateDDSTextureFromMemory(pRender->m_pDevice, m_TexData, m_DataSize, &pResource, &m_TexInfo->SRView, 0, TNULL);
-				}
-			}
+                if (data != NULL)
+                {
+                    m_TexInfo->SRView = pRender->CreateTexture(width, height, DXGI_FORMAT_R8G8B8A8_UNORM, data, 0, D3D11_USAGE_IMMUTABLE, 0, 1);
+                    if (m_TexInfo->SRView) pResource = TNULL;
+                    stbi_image_free(data);
+                }
+                else
+                {
+                    DirectX::CreateDDSTextureFromMemory(pRender->m_pDevice, m_TexData, m_DataSize, &pResource, &m_TexInfo->SRView, 0, TNULL);
+                }
+            }
 
-			pFile->Destroy();
-		}
-		else
-		{
-			DirectX::CreateDDSTextureFromMemory(pRender->m_pDevice, m_TexData, m_DataSize, &pResource, &m_TexInfo->SRView, 0, TNULL);
-		}
+            pFile->Destroy();
+        }
+        else
+        {
+            DirectX::CreateDDSTextureFromMemory(pRender->m_pDevice, m_TexData, m_DataSize, &pResource, &m_TexInfo->SRView, 0, TNULL);
+        }
 
-		if (pResource != TNULL)
-		{
-			pResource->Release();
-		}
-		else
-		{
-			TASSERT(TFALSE);
-			return;
-		}
+        if (pResource != TNULL)
+        {
+            pResource->Release();
+        }
+        else
+        {
+            TASSERT(TFALSE);
+            return;
+        }
 
-		TTextureManager::GetSingletonWeak()->AddTexture(this);
+        TTextureManager::GetSingletonWeak()->AddTexture(this);
         SelectSamplerId();
 
 #ifdef TOSHI_DEBUG
         TTextureManager::s_NumTextures += 1;
 #endif
-	}
+    }
 
     void TTexture::Deinit()
     {
-		m_TexInfo->SRView->Release();
+        m_TexInfo->SRView->Release();
 
-		if (m_pPrevTexture)
-			m_pPrevTexture->m_pNextTexture = m_pNextTexture;
+        if (m_pPrevTexture)
+            m_pPrevTexture->m_pNextTexture = m_pNextTexture;
 
         if (m_pNextTexture)
             m_pNextTexture->m_pPrevTexture = m_pPrevTexture;
 
-		auto pTexManager = TTextureManager::GetSingletonWeak();
+        auto pTexManager = TTextureManager::GetSingletonWeak();
 
-		if (pTexManager->GetLastTexture() == this)
-			pTexManager->SetLastTexture(m_pPrevTexture);
+        if (pTexManager->GetLastTexture() == this)
+            pTexManager->SetLastTexture(m_pPrevTexture);
 
         m_pNextTexture = TNULL;
         m_pPrevTexture = TNULL;
 
 #ifdef TOSHI_DEBUG
-		TASSERT(TTextureManager::s_NumTextures > 0);
+        TASSERT(TTextureManager::s_NumTextures > 0);
         TTextureManager::s_NumTextures -= 1;
 #endif
     }
 
     void TTexture::Bind(UINT startSlot)
-	{
-		auto pRender = TRenderDX11::Interface();
-		pRender->m_pDeviceContext->PSSetShaderResources(startSlot, 1, &m_TexInfo->SRView);
-		pRender->SetSamplerState(startSlot, m_SamplerId, TRUE);
-	}
+    {
+        auto pRender = TRenderDX11::Interface();
+        pRender->m_pDeviceContext->PSSetShaderResources(startSlot, 1, &m_TexInfo->SRView);
+        pRender->SetSamplerState(startSlot, m_SamplerId, TRUE);
+    }
 
-	TTexture* TTexture::InitRunTime(Info* pTextureInfo)
-	{
-		TRenderDX11* pRender = TRenderDX11::Interface();
+    TTexture* TTexture::InitRunTime(Info* pTextureInfo)
+    {
+        TRenderDX11* pRender = TRenderDX11::Interface();
 
-		TTexture* pTexture = new TTexture;
-		TASSERT(pTexture != TNULL);
+        TTexture* pTexture = new TTexture;
+        TASSERT(pTexture != TNULL);
 
-		pTexture->m_TexName = "runtime";
-		pTexture->m_TexInfo = pTextureInfo;
-		pTexture->m_TexData = TNULL;
-		TTextureManager::GetSingletonWeak()->AddTexture(pTexture);
+        pTexture->m_TexName = "runtime";
+        pTexture->m_TexInfo = pTextureInfo;
+        pTexture->m_TexData = TNULL;
+        TTextureManager::GetSingletonWeak()->AddTexture(pTexture);
 
-		return pTexture;
-	}
+        return pTexture;
+    }
 
-	TTexture* TTexture::InitRunTime(DXGI_FORMAT format, UINT width, UINT height, const void* srcData)
-	{
-		TRenderDX11* pRender = TRenderDX11::Interface();
+    TTexture* TTexture::InitRunTime(DXGI_FORMAT format, UINT width, UINT height, const void* srcData)
+    {
+        TRenderDX11* pRender = TRenderDX11::Interface();
 
-		TTexture* pTexture = new TTexture;
-		TASSERT(pTexture != TNULL);
+        TTexture* pTexture = new TTexture;
+        TASSERT(pTexture != TNULL);
 
-		pTexture->m_TexName = "runtime";
+        pTexture->m_TexName = "runtime";
 
-		pTexture->m_TexInfo = new TTexture::Info;
-		TASSERT(pTexture->m_TexInfo != TNULL);
+        pTexture->m_TexInfo = new TTexture::Info;
+        TASSERT(pTexture->m_TexInfo != TNULL);
 
-		pTexture->m_TexInfo->SRView = pRender->CreateTexture(width, height, format, srcData, 0, D3D11_USAGE_IMMUTABLE, 0, 1);
-		pTexture->m_TexInfo->Width = width;
-		pTexture->m_TexInfo->Height = height;
-		pTexture->m_TexInfo->MipMapCount = 1;
-		pTexture->m_TexInfo->Format = format;
+        pTexture->m_TexInfo->SRView = pRender->CreateTexture(width, height, format, srcData, 0, D3D11_USAGE_IMMUTABLE, 0, 1);
+        pTexture->m_TexInfo->Width = width;
+        pTexture->m_TexInfo->Height = height;
+        pTexture->m_TexInfo->MipMapCount = 1;
+        pTexture->m_TexInfo->Format = format;
 
-		TTextureManager::GetSingletonWeak()->AddTexture(pTexture);
+        TTextureManager::GetSingletonWeak()->AddTexture(pTexture);
 
-		return pTexture;
-	}
+        return pTexture;
+    }
 
-	void TTexture::SelectSamplerId()
-	{
+    void TTexture::SelectSamplerId()
+    {
         if (m_eAddressU == D3D11_TEXTURE_ADDRESS_CLAMP && m_eAddressV == D3D11_TEXTURE_ADDRESS_CLAMP)
         {
             if (m_eFilter == D3D11_FILTER_MIN_MAG_MIP_POINT)
@@ -192,7 +192,7 @@ namespace Toshi
                 m_SamplerId = 3;
             }
         }
-	}
+    }
 
     void TTexture::SetWrap(D3D11_TEXTURE_ADDRESS_MODE eAddressU, D3D11_TEXTURE_ADDRESS_MODE eAddressV)
     {
@@ -202,80 +202,80 @@ namespace Toshi
     }
 
     TTextureManager::TTextureManager() :
-		m_pLastTexture(TNULL)
-	{
-		uint8_t* srcData;
+        m_pLastTexture(TNULL)
+    {
+        uint8_t* srcData;
 
-		// White texture
-		constexpr int WhiteTextureWidth = 8;
-		constexpr int WhiteTextureHeight = 8;
-		constexpr int WhiteTextureSize = WhiteTextureWidth * WhiteTextureHeight * 4;
+        // White texture
+        constexpr int WhiteTextureWidth = 8;
+        constexpr int WhiteTextureHeight = 8;
+        constexpr int WhiteTextureSize = WhiteTextureWidth * WhiteTextureHeight * 4;
 
-		srcData = (uint8_t*)TMemalign(16, WhiteTextureSize);
-		TUtil::MemSet(srcData, 0xFF, WhiteTextureSize);
-		m_pWhiteTexture = TTexture::InitRunTime(DXGI_FORMAT_R8G8B8A8_UNORM, WhiteTextureWidth, WhiteTextureHeight, srcData);
-		m_pWhiteTexture->SetName("white");
-		m_pWhiteTexture->m_pNextTexture = TNULL;
-		m_pWhiteTexture->m_pPrevTexture = TNULL;
-		TFree(srcData);
-		
-		// Invalid texture
-		constexpr int InvalidTextureWidth = 128;
-		constexpr int InvalidTextureHeight = 128;
-		constexpr int InvalidTextureSize = InvalidTextureWidth * InvalidTextureHeight * 4;
+        srcData = (uint8_t*)TMemalign(16, WhiteTextureSize);
+        TUtil::MemSet(srcData, 0xFF, WhiteTextureSize);
+        m_pWhiteTexture = TTexture::InitRunTime(DXGI_FORMAT_R8G8B8A8_UNORM, WhiteTextureWidth, WhiteTextureHeight, srcData);
+        m_pWhiteTexture->SetName("white");
+        m_pWhiteTexture->m_pNextTexture = TNULL;
+        m_pWhiteTexture->m_pPrevTexture = TNULL;
+        TFree(srcData);
+        
+        // Invalid texture
+        constexpr int InvalidTextureWidth = 128;
+        constexpr int InvalidTextureHeight = 128;
+        constexpr int InvalidTextureSize = InvalidTextureWidth * InvalidTextureHeight * 4;
 
-		srcData = (uint8_t*)TMemalign(16, InvalidTextureSize);
-		
-		for (uint8_t* pos = srcData; pos < srcData; pos += 4)
-		{
-			*(pos) = rand() % 256;
-			*(pos + 1) = rand() % 256;
-			*(pos + 2) = rand() % 256;
-			*(pos + 3) = rand() % 256;
-		}
+        srcData = (uint8_t*)TMemalign(16, InvalidTextureSize);
+        
+        for (uint8_t* pos = srcData; pos < srcData; pos += 4)
+        {
+            *(pos) = rand() % 256;
+            *(pos + 1) = rand() % 256;
+            *(pos + 2) = rand() % 256;
+            *(pos + 3) = rand() % 256;
+        }
 
-		m_pInvalidTexture = TTexture::InitRunTime(DXGI_FORMAT_R8G8B8A8_UNORM, 8, 8, srcData);
-		m_pInvalidTexture->SetName("invalid");
-		m_pInvalidTexture->m_pNextTexture = TNULL;
-		m_pInvalidTexture->m_pPrevTexture = TNULL;
-		TFree(srcData);
-	}
+        m_pInvalidTexture = TTexture::InitRunTime(DXGI_FORMAT_R8G8B8A8_UNORM, 8, 8, srcData);
+        m_pInvalidTexture->SetName("invalid");
+        m_pInvalidTexture->m_pNextTexture = TNULL;
+        m_pInvalidTexture->m_pPrevTexture = TNULL;
+        TFree(srcData);
+    }
 
-	TTexture* TTextureManager::FindTexture(const char* texName)
-	{
-		TASSERT(T2String8::IsLowerCase(texName));
+    TTexture* TTextureManager::FindTexture(const char* texName)
+    {
+        TASSERT(T2String8::IsLowerCase(texName));
 
-		TTexture* pCurrentTex = m_pLastTexture;
+        TTexture* pCurrentTex = m_pLastTexture;
 
-		while (TTRUE)
-		{
-			if (pCurrentTex == TNULL)
-			{
-				return m_pInvalidTexture;
-			}
+        while (TTRUE)
+        {
+            if (pCurrentTex == TNULL)
+            {
+                return m_pInvalidTexture;
+            }
 
-			if (T2String8::CompareNoCase(pCurrentTex->GetName(), texName) == 0)
-			{
-				break;
-			}
+            if (T2String8::CompareNoCase(pCurrentTex->GetName(), texName) == 0)
+            {
+                break;
+            }
 
-			pCurrentTex = pCurrentTex->m_pPrevTexture;
-		}
+            pCurrentTex = pCurrentTex->m_pPrevTexture;
+        }
 
-		return pCurrentTex;
-	}
+        return pCurrentTex;
+    }
 
-	void TTextureManager::AddTexture(TTexture* pTexture)
-	{
-		pTexture->m_pPrevTexture = GetLastTexture();
-		pTexture->m_pNextTexture = TNULL;
+    void TTextureManager::AddTexture(TTexture* pTexture)
+    {
+        pTexture->m_pPrevTexture = GetLastTexture();
+        pTexture->m_pNextTexture = TNULL;
 
-		if (pTexture->m_pPrevTexture != TNULL)
-		{
-			pTexture->m_pPrevTexture->m_pNextTexture = pTexture;
-		}
+        if (pTexture->m_pPrevTexture != TNULL)
+        {
+            pTexture->m_pPrevTexture->m_pNextTexture = pTexture;
+        }
 
-		SetLastTexture(pTexture);
-	}
+        SetLastTexture(pTexture);
+    }
 
 }
--- Toshi/Source/Platform/Windows/DX11/TTexture_DX11.h
+++ Toshi/Source/Platform/Windows/DX11/TTexture_DX11.h
@@ -3,79 +3,79 @@
 
 namespace Toshi
 {
-	class TTexture
-	{
-	public:
-		friend class TTextureManager;
+    class TTexture
+    {
+    public:
+        friend class TTextureManager;
 
-		struct Info
-		{
-			UINT Width;
-			UINT Height;
-			UINT MipMapCount;
-			UINT16 Width2;
-			UINT16 Height2;
-			DXGI_FORMAT Format;
-			ID3D11ShaderResourceView* SRView;
+        struct Info
+        {
+            UINT Width;
+            UINT Height;
+            UINT MipMapCount;
+            UINT16 Width2;
+            UINT16 Height2;
+            DXGI_FORMAT Format;
+            ID3D11ShaderResourceView* SRView;
 
-			Info()
-			{
-				Width = 0;
-				Height = 0;
-				MipMapCount = 0;
-				Width2 = 0;
-				Height2 = 0;
-				Format = DXGI_FORMAT_UNKNOWN;
-				SRView = TNULL;
-			}
-		};
+            Info()
+            {
+                Width = 0;
+                Height = 0;
+                MipMapCount = 0;
+                Width2 = 0;
+                Height2 = 0;
+                Format = DXGI_FORMAT_UNKNOWN;
+                SRView = TNULL;
+            }
+        };
 
-	public:
-		TTexture()
-		{
-			m_Unk1 = TNULL;
-			m_TexInfo = TNULL;
-			m_TexName = TNULL;
-			m_DataSize = 0;
-			m_TexData = 0;
-			m_pPrevTexture = TNULL;
-			m_pNextTexture = TNULL;
-			m_Flags1 = 0;
+    public:
+        TTexture()
+        {
+            m_Unk1 = TNULL;
+            m_TexInfo = TNULL;
+            m_TexName = TNULL;
+            m_DataSize = 0;
+            m_TexData = 0;
+            m_pPrevTexture = TNULL;
+            m_pNextTexture = TNULL;
+            m_Flags1 = 0;
             m_eAddressU = D3D11_TEXTURE_ADDRESS_WRAP;
             m_eAddressV = D3D11_TEXTURE_ADDRESS_WRAP;
             m_eFilter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
             m_SamplerId = 3;
-		}
+        }
 
         void Init();
         void Deinit();
-		void Bind(UINT startSlot);
-		TTexture* InitRunTime(Info* pTextureInfo);
-		static TTexture* InitRunTime(DXGI_FORMAT format, UINT width, UINT height, const void* srcData);
-		void SelectSamplerId();
+        void Bind(UINT startSlot);
+        TTexture* InitRunTime(Info* pTextureInfo);
+        static TTexture* InitRunTime(DXGI_FORMAT format, UINT width, UINT height, const void* srcData);
+        void SelectSamplerId();
 
-		const char* GetName() const       { return m_TexName; }
-		uint8_t GetFlags1() const         { return m_Flags1; }
-		void* GetHandle() const           { return m_TexInfo->SRView; }
+        const char* GetName() const       { return m_TexName; }
+        uint8_t GetFlags1() const         { return m_Flags1; }
+        void* GetHandle() const           { return m_TexInfo->SRView; }
         UINT GetWidth() const             { return m_TexInfo->Width; }
-		UINT GetHeight() const            { return m_TexInfo->Height; }
+        UINT GetHeight() const            { return m_TexInfo->Height; }
         
         TTexture* GetPrev() const         { return m_pPrevTexture; }
         TTexture* GetNext() const         { return m_pNextTexture; }
-		
+        
         void SetName(const char* name)    { m_TexName = name; }
-		void SetSamplerId(int iSamplerId) { m_SamplerId = iSamplerId; }
+        void SetSamplerId(int iSamplerId) { m_SamplerId = iSamplerId; }
         void SetWrap(D3D11_TEXTURE_ADDRESS_MODE eAddressU, D3D11_TEXTURE_ADDRESS_MODE eAddressV);
 
-		void SetAlphaEnabled(TBOOL bEnabled)
-		{
-			if (bEnabled)
-				m_Flags1 |= 1;
-			else
-				m_Flags1 &= ~1;
-		}
+        void SetAlphaEnabled(TBOOL bEnabled)
+        {
+            if (bEnabled)
+                m_Flags1 |= 1;
+            else
+                m_Flags1 &= ~1;
+        }
 
-	private:
+    private:
         void* m_Unk1;                           // 0x00
         const char* m_TexName;                  // 0x04
         Info* m_TexInfo;                        // 0x08
@@ -87,56 +87,56 @@ namespace Toshi
         D3D11_TEXTURE_ADDRESS_MODE m_eAddressU; // 0x20
         D3D11_TEXTURE_ADDRESS_MODE m_eAddressV; // 0x24
         D3D11_FILTER m_eFilter;                 // 0x28
-		int m_SamplerId;                        // 0x2C
-	};
+        int m_SamplerId;                        // 0x2C
+    };
 
-	class TTextureManager : public TSingleton<TTextureManager>
-	{
-	public:
-		friend class TTexture;
+    class TTextureManager : public TSingleton<TTextureManager>
+    {
+    public:
+        friend class TTexture;
 
-	public:
-		TTextureManager();
-		~TTextureManager() = default;
+    public:
+        TTextureManager();
+        ~TTextureManager() = default;
 
-		void SetLastTexture(TTexture* pTexture)
-		{
-			m_pLastTexture = pTexture;
-		}
+        void SetLastTexture(TTexture* pTexture)
+        {
+            m_pLastTexture = pTexture;
+        }
 
-		TTexture* GetLastTexture() const
-		{
-			return m_pLastTexture;
-		}
+        TTexture* GetLastTexture() const
+        {
+            return m_pLastTexture;
+        }
 
-		TTexture* GetInvalidTexture() const
-		{
-			return m_pInvalidTexture;
-		}
+        TTexture* GetInvalidTexture() const
+        {
+            return m_pInvalidTexture;
+        }
 
-		TTexture* GetWhiteTexture() const
-		{
-			return m_pWhiteTexture;
-		}
+        TTexture* GetWhiteTexture() const
+        {
+            return m_pWhiteTexture;
+        }
 
-		TTexture* FindTexture(const char* texName);
+        TTexture* FindTexture(const char* texName);
 
-		void AddTexture(TTexture* pTexture);
+        void AddTexture(TTexture* pTexture);
 
 #ifdef TOSHI_DEBUG
-	public:
+    public:
         size_t DEBUG_GetNumTextures() const
         {
             return s_NumTextures;
         }
 
-	private:
-		inline static size_t s_NumTextures = 0;
+    private:
+        inline static size_t s_NumTextures = 0;
 #endif // TOSHI_DEBUG
 
-	private:
-		TTexture* m_pLastTexture;
-		TTexture* m_pInvalidTexture;
-		TTexture* m_pWhiteTexture;
-	};
+    private:
+        TTexture* m_pLastTexture;
+        TTexture* m_pInvalidTexture;
+        TTexture* m_pWhiteTexture;
+    };
 }
