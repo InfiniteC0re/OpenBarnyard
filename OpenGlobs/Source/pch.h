10dc171674c0e0c442ee179fabada268c586d306
AdventureT <le@nepelius.at>
2023-06-29 19:42:35 +0200

    Make OpenGlobs and OpenJPOG compilable

--- OpenGlobs/Source/pch.h
+++ OpenGlobs/Source/pch.h
@@ -12,14 +12,17 @@
 
 /* Toshi Header Files */
 #include <Toshi/Typedefs.h>
-#include "Toshi/Core/STL.h"
+#include <Toshi/Memory/TMemory.h>
+#include <Toshi/Core/STL.h>
 #include <Toshi/Core/Core.h>
 #include <Toshi/Core/TObject.h>
-#include "Toshi/Core/TStack.h"
 #include <Toshi/Core/TTask.h>
-#include <Toshi/Memory/TMemory.h>
 #include <Toshi/Math/Math.h>
 #include <Toshi/Utils/TLog.h>
+#include <Toshi/Utils/TSingleton.h>
 #include <Toshi/Utils/TUtil.h>
+#include <Toshi/Thread/TMutexLock.h>
 #include <Toshi/Strings/TStringManager.h>
 
+/* Toshi 2.0 Header Files */
+#include "Toshi2/T2Flags.h"
