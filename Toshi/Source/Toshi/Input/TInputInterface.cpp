de659c46788179542fab524681d7e8c33556b882
InfiniteC0re <inf.core0@gmail.com>
2023-07-07 15:58:21 +0500

    Create TInputDXDeviceKeyboard in the input interface

--- Toshi/Source/Toshi/Input/TInputInterface.cpp
+++ Toshi/Source/Toshi/Input/TInputInterface.cpp
@@ -5,121 +5,121 @@
 
 namespace Toshi
 {
-    TInputDevice* TInputInterface::GetDeviceByIndex(TClass* pClass, size_t index)
-    {
-        if (m_DeviceList.Count() == 0) return TNULL;
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            if (it->GetClass()->IsA(pClass))
-            {
-                if (index == 0)
-                {
-                    return it;
-                }
-                else
-                {
-                    index -= 1;
-                }
-            }
-        }
+	TInputDevice* TInputInterface::GetDeviceByIndex(TClass* pClass, size_t index)
+	{
+		if (m_DeviceList.Count() == 0) return TNULL;
+		for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
+		{
+			if (it->GetClass()->IsA(pClass))
+			{
+				if (index == 0)
+				{
+					return it;
+				}
+				else
+				{
+					index -= 1;
+				}
+			}
+		}
 
-        return TNULL;
-    }
+		return TNULL;
+	}
 
-    void TInputInterface::AddDevice(TInputDevice* device)
-    {
-        if (device != TNULL)
-        {
-            m_DeviceList.InsertTail(*device);
-            device->SetInputInterface(this);
-        }
-    }
+	void TInputInterface::AddDevice(TInputDevice* device)
+	{
+		if (device != TNULL)
+		{
+			m_DeviceList.InsertTail(*device);
+			device->SetInputInterface(this);
+		}
+	}
 
-    void TInputInterface::RemoveDevice(TInputDevice* device)
-    {
-        if (device != TNULL)
-        {
-            m_DeviceList.Remove(*device);
-            device->SetInputInterface(TNULL);
-        }
-    }
+	void TInputInterface::RemoveDevice(TInputDevice* device)
+	{
+		if (device != TNULL)
+		{
+			m_DeviceList.Remove(*device);
+			device->SetInputInterface(TNULL);
+		}
+	}
 
-    TBOOL TInputInterface::Deinitialise()
-    {
-        m_DeviceList.DeleteAll();
-        return TTRUE;
-    }
+	TBOOL TInputInterface::Deinitialise()
+	{
+		m_DeviceList.DeleteAll();
+		return TTRUE;
+	}
 
-    TBOOL TInputInterface::AcquireAll()
-    {
-        TBOOL bRet = TTRUE;
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            bRet = it->Acquire();
-        }
-        return bRet;
-    }
+	TBOOL TInputInterface::AcquireAll()
+	{
+		TBOOL bRet = TTRUE;
+		for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
+		{
+			bRet = it->Acquire();
+		}
+		return bRet;
+	}
 
-    TBOOL TInputInterface::UnacquireAll()
-    {
-        TBOOL bRet = TTRUE;
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            bRet = it->Unacquire();
-        }
-        return bRet;
-    }
+	TBOOL TInputInterface::UnacquireAll()
+	{
+		TBOOL bRet = TTRUE;
+		for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
+		{
+			bRet = it->Unacquire();
+		}
+		return bRet;
+	}
 
-    TBOOL TInputInterface::FlushAll()
-    {
-        TBOOL bRet = TTRUE;
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            bRet = it->Flush();
-        }
-        return bRet;
-    }
+	TBOOL TInputInterface::FlushAll()
+	{
+		TBOOL bRet = TTRUE;
+		for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
+		{
+			bRet = it->Flush();
+		}
+		return bRet;
+	}
 
-    void TInputInterface::SetExclusiveMode(TBOOL mode)
-    {
-        m_bIsExclusiveMode = mode;
-    }
+	void TInputInterface::SetExclusiveMode(TBOOL mode)
+	{
+		m_bIsExclusiveMode = mode;
+	}
 
-    TBOOL TInputInterface::GetExclusiveMode() const
-    {
-        return m_bIsExclusiveMode;
-    }
+	TBOOL TInputInterface::GetExclusiveMode() const
+	{
+		return m_bIsExclusiveMode;
+	}
 
-    int TInputInterface::ProcessEvents(float deltaTime)
-    {
-        int events = 0;
+	int TInputInterface::ProcessEvents(float deltaTime)
+	{
+		int iNumProcessed = 0;
 
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            if (it->IsAcquired())
-            {
-                it->Update(deltaTime);
-            }
-        }
+		for (auto it = m_DeviceList.Tail(); it != m_DeviceList.End(); --it)
+		{
+			if (it->IsAcquired())
+			{
+				it->Update(deltaTime);
+			}
+		}
 
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            if (it->IsAcquired())
-            {
-                events += it->ProcessEvents(m_Emitter1, deltaTime);
-            }
-        }
+		for (auto it = m_DeviceList.Tail(); it != m_DeviceList.End(); --it)
+		{
+			if (it->IsAcquired())
+			{
+				iNumProcessed += it->ProcessEvents(m_Emitter1, deltaTime);
+			}
+		}
 
-        return events;
-    }
+		return iNumProcessed;
+	}
 
-    void TInputInterface::StopAllRepeats()
-    {
-        for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
-        {
-            it->StopAllRepeats();
-        }
-    }
+	void TInputInterface::StopAllRepeats()
+	{
+		for (auto it = m_DeviceList.Begin(); it != m_DeviceList.End(); ++it)
+		{
+			it->StopAllRepeats();
+		}
+	}
 
 	TInputInterface::InputEvent::InputEvent(TInputDevice* a_pDevice, int a_iDoodad, EventType a_eEventType)
 	{
