129cc2ecb0f60376e61ef0c0d23ad5c9d7be084e
AdventureT <le@nepelius.at>
2023-08-08 12:12:26 +0200

    A bit of PProperty impl

--- Toshi/Source/Toshi/Properties/PProperty.h
+++ Toshi/Source/Toshi/Properties/PProperty.h
@@ -7,13 +7,48 @@ class PPropertyName
 
 	PPropertyName()
 	{
-		m_pName = TNULL;
-		m_pSubName = TNULL;
+		m_pName.m_pPooledString = TNULL;
+		m_pSubName.m_pPooledString = TNULL;
 	}
 
+	PPropertyName(const Toshi::TPCString& a_pName, const Toshi::TPCString& a_pSubName)
+	{
+		m_pName.m_pPooledString = TNULL;
+		if (a_pName.m_pPooledString)
+		{
+			m_pName.m_pPooledString = a_pName.m_pPooledString;
+			a_pName.m_pPooledString->m_iCount++;
+		}
+		m_pSubName.m_pPooledString = TNULL;
+		if (a_pSubName.m_pPooledString)
+		{
+			m_pSubName.m_pPooledString = a_pSubName.m_pPooledString;
+			a_pSubName.m_pPooledString->m_iCount++;
+		}
+	}
 
-	Toshi::TPCString* m_pName;
-	Toshi::TPCString* m_pSubName;
+	PPropertyName(const PPropertyName& other)
+	{
+		m_pName.m_pPooledString = TNULL;
+		if (other.m_pName.m_pPooledString)
+		{
+			m_pName.m_pPooledString = other.m_pName.m_pPooledString;
+			other.m_pName.m_pPooledString->m_iCount++;
+		}
+		m_pSubName.m_pPooledString = TNULL;
+		if (other.m_pSubName.m_pPooledString)
+		{
+			m_pSubName.m_pPooledString = other.m_pSubName.m_pPooledString;
+			other.m_pSubName.m_pPooledString->m_iCount++;
+		}
+	}
+
+protected:
+	void Split(const Toshi::TPCString& a_pName);
+
+
+	Toshi::TPCString m_pName;
+	Toshi::TPCString m_pSubName;
 };
 
 class PPropertyBlock
