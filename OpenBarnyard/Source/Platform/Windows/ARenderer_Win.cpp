de7835b713dde7412568b9eb42d84c79b45017db
InfiniteC0re <inf.core0@gmail.com>
2023-11-12 02:29:57 +0500

    Fix rendered textures being flipped

--- OpenBarnyard/Source/Platform/Windows/ARenderer_Win.cpp
+++ OpenBarnyard/Source/Platform/Windows/ARenderer_Win.cpp
@@ -191,11 +191,11 @@ void ARenderer::RenderGUI()
 	AGUI2::GetContext()->GetRenderer()->SetColour(TCOLOR(255, 255, 255));
 
 	AGUI2::GetContext()->GetRenderer()->SetMaterial(AGUI2TextureSectionManager::GetTextureSection("Panel_01")->GetMaterial());
-	AGUI2::GetContext()->GetRenderer()->RenderRectangle({ -80.0f, -25.0f }, { 80.0f, 25.0f }, { 1.0f, 1.0f }, { 0, 0 });
+	AGUI2::GetContext()->GetRenderer()->RenderRectangle({ 80.0f, 25.0f }, { -80.0f, -25.0f }, { 1.0f, 1.0f }, { 0, 0 });
 
 	auto pSection = AGUI2TextureSectionManager::GetTextureSection("Putt_Ball");
 	AGUI2::GetContext()->GetRenderer()->SetMaterial(pSection->GetMaterial());
-	AGUI2::GetContext()->GetRenderer()->RenderRectangle({ -10.0f, -10.0f }, { 10.0f, 10.0f }, { pSection->GetUVPoint2().x, pSection->GetUVPoint2().y }, { pSection->GetUVPoint1().x, pSection->GetUVPoint1().y });
+	AGUI2::GetContext()->GetRenderer()->RenderRectangle({ 10.0f, 10.0f }, { -10.0f, -10.0f }, { pSection->GetUVPoint2().x, pSection->GetUVPoint2().y }, { pSection->GetUVPoint1().x, pSection->GetUVPoint1().y });
 
 	m_RenderGUIEmitter.Throw(0);
 	AGUI2::GetContext()->GetRenderer()->EndScene();
