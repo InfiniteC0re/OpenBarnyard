a86819c1d6e44e4c8d30755980a413f3574283bc
InfiniteC0re <inf.core0@gmail.com>
2025-01-06 20:48:37 +0500

    Implement T2VertexArray

--- Toshi/Source/Platform/GL/T2RenderBuffer_GL.h
+++ Toshi/Source/Platform/GL/T2RenderBuffer_GL.h
@@ -16,13 +16,16 @@ class T2RenderBuffer
 {
 public:
 	constexpr T2RenderBuffer()
-	    : m_uiId( 0 ) {}
+	    : m_uiId( 0 )
+	{}
 
 	constexpr T2RenderBuffer( GLuint a_uiId )
-	    : m_uiId( a_uiId ) {}
+	    : m_uiId( a_uiId )
+	{}
 
 	constexpr T2RenderBuffer( const T2RenderBuffer& a_rOther )
-	    : m_uiId( a_rOther.m_uiId ) {}
+	    : m_uiId( a_rOther.m_uiId )
+	{}
 
 	constexpr GLuint GetId() const { return m_uiId; }
 
@@ -45,6 +48,67 @@ private:
 
 using T2VertexBuffer = T2RenderBuffer<GL_ARRAY_BUFFER>;
 using T2IndexBuffer  = T2RenderBuffer<GL_ELEMENT_ARRAY_BUFFER>;
+
+class T2VertexArray
+{
+public:
+	constexpr T2VertexArray()
+	    : m_uiId( 0 )
+	{}
+
+	constexpr T2VertexArray( GLuint a_uiId )
+	    : m_uiId( a_uiId )
+	{}
+
+	constexpr T2VertexArray( GLuint a_uiId, T2VertexBuffer a_VertexBuffer, T2IndexBuffer a_IndexBuffer )
+	    : m_uiId( a_uiId ), m_VertexBuffer( a_VertexBuffer ), m_IndexBuffer( a_IndexBuffer )
+	{}
+
+	constexpr T2VertexArray( const T2VertexArray& a_rOther )
+	    : m_uiId( a_rOther.m_uiId ), m_VertexBuffer( a_rOther.m_VertexBuffer ), m_IndexBuffer( a_rOther.m_IndexBuffer )
+	{}
+
+	void SetAttribPointer( GLuint a_uiIndex, GLint a_iNumComponents, GLenum a_eType, GLsizei a_iStride, const void* a_pOffset, GLboolean a_bNormalized = GL_FALSE )
+	{
+		TASSERT( m_uiId != 0 );
+		glBindVertexArray( m_uiId );
+		glBindBuffer( GL_ARRAY_BUFFER, m_VertexBuffer.GetId() );
+		glVertexAttribPointer( a_uiIndex, a_iNumComponents, a_eType, a_bNormalized, a_iStride, a_pOffset );
+		glEnableVertexAttribArray( a_uiIndex );
+	}
+
+	void SetVertexBuffer( T2VertexBuffer a_VertexBuffer )
+	{
+		TASSERT( m_uiId != 0 );
+		glBindVertexArray( m_uiId );
+		a_VertexBuffer.Bind();
+		m_VertexBuffer = a_VertexBuffer;
+	}
+
+	void SetIndexBuffer( T2IndexBuffer a_IndexBuffer )
+	{
+		TASSERT( m_uiId != 0 );
+		glBindVertexArray( m_uiId );
+		a_IndexBuffer.Bind();
+		m_IndexBuffer = a_IndexBuffer;
+	}
+
+	constexpr GLuint GetId() const { return m_uiId; }
+
+	void Bind() { glBindVertexArray( m_uiId ); }
+	void Unbind() { glBindVertexArray( 0 ); }
+	void Destroy() { glDeleteVertexArrays( 1, &m_uiId ); }
+
+	constexpr T2VertexBuffer GetVertexBuffer() { return m_VertexBuffer; }
+	constexpr T2IndexBuffer  GetIndexBuffer() { return m_IndexBuffer; }
+
+	constexpr operator TBOOL() const { return m_uiId != 0; }
+
+private:
+	GLuint         m_uiId;
+	T2VertexBuffer m_VertexBuffer;
+	T2IndexBuffer  m_IndexBuffer;
+};
 
 TOSHI_NAMESPACE_END
 
--- Toshi/Source/Platform/GL/T2Render_GL.cpp
+++ Toshi/Source/Platform/GL/T2Render_GL.cpp
@@ -1,6 +1,5 @@
 #include "ToshiPCH.h"
 #include "Render/T2Render.h"
-#include "T2RenderBuffer_GL.h"
 
 //-----------------------------------------------------------------------------
 // Enables memory debugging.
@@ -27,6 +26,60 @@ TBOOL T2Render::Create( const WindowParams& a_rcWindowParams )
 
 void T2Render::Destroy()
 {
+}
+
+T2VertexBuffer T2Render::CreateVertexBuffer( const void* a_pData, GLuint a_uiSize, GLenum a_eUsage )
+{
+	GLuint uiBufferId;
+	glGenBuffers( 1, &uiBufferId );
+
+	TASSERT( uiBufferId != 0 );
+	T2VertexBuffer buffer( uiBufferId );
+
+	if ( a_uiSize != 0 )
+	{
+		buffer.SetData( a_pData, a_uiSize, a_eUsage );
+	}
+
+	return buffer;
+}
+
+T2IndexBuffer T2Render::CreateIndexBuffer( const TUINT16* a_pIndices, GLuint a_uiCount, GLenum a_eUsage )
+{
+	GLuint uiBufferId;
+	glGenBuffers( 1, &uiBufferId );
+
+	TASSERT( uiBufferId != 0 );
+	T2IndexBuffer buffer( uiBufferId );
+
+	if ( a_uiCount != 0 )
+	{
+		buffer.SetData( a_pIndices, sizeof( *a_pIndices ) * a_uiCount, a_eUsage );
+	}
+
+	return buffer;
+}
+
+T2VertexArray T2Render::CreateVertexArray()
+{
+	GLuint uiVaoId;
+	glGenVertexArrays( 1, &uiVaoId );
+
+	TASSERT( uiVaoId != 0 );
+	return uiVaoId;
+}
+
+T2VertexArray T2Render::CreateVertexArray( T2VertexBuffer a_VertexBuffer, T2IndexBuffer a_IndexBuffer )
+{
+	GLuint uiVaoId;
+	glGenVertexArrays( 1, &uiVaoId );
+	TASSERT( uiVaoId != 0 );
+
+	T2VertexArray vao( uiVaoId );
+	vao.SetVertexBuffer( a_VertexBuffer );
+	vao.SetIndexBuffer( a_IndexBuffer );
+
+	return vao;
 }
 
 TOSHI_NAMESPACE_END
