cd21746b5c5de8744078e9cb404180361cc98e00
InfiniteC0re <inf.core0@gmail.com>
2025-01-06 20:48:37 +0500

    Implement T2VertexArray

--- Toshi/Source/Platform/GL/T2Render_GL.h
+++ Toshi/Source/Platform/GL/T2Render_GL.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #ifdef TRENDERINTERFACE_GL
-#include "Render/T2RenderCommon.h"
+#  include "Render/T2RenderCommon.h"
+#  include "T2RenderBuffer_GL.h"
 
 TOSHI_NAMESPACE_START
 
@@ -18,9 +19,9 @@ public:
 	struct WindowParams
 	{
 		const TCHAR* pchTitle;
-		TUINT uiWidth;
-		TUINT uiHeight;
-		TBOOL bIsWindowed;
+		TUINT        uiWidth;
+		TUINT        uiHeight;
+		TBOOL        bIsWindowed;
 	};
 
 public:
@@ -32,6 +33,12 @@ public:
 
 	// Destroys renderer and everything it manages
 	void Destroy();
+
+public:
+	static T2VertexBuffer CreateVertexBuffer( const void* a_pData, GLuint a_uiSize, GLenum a_eUsage );
+	static T2IndexBuffer  CreateIndexBuffer( const TUINT16* a_pIndices, GLuint a_uiCount, GLenum a_eUsage );
+	static T2VertexArray  CreateVertexArray();
+	static T2VertexArray  CreateVertexArray( T2VertexBuffer a_VertexBuffer, T2IndexBuffer a_IndexBuffer );
 
 private:
 	void OnDeviceReset();
