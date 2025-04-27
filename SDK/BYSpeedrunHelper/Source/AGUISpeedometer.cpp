6696325b0180b3290e4e77d0f9d87137285b303e
InfiniteC0re <inf.core0@gmail.com>
2025-04-27 18:10:10 +0500

    [BYardSDK] Add ASteer

--- SDK/BYSpeedrunHelper/Source/AGUISpeedometer.cpp
+++ SDK/BYSpeedrunHelper/Source/AGUISpeedometer.cpp
@@ -3,6 +3,7 @@
 #include "HookHelpers.h"
 #include "AModSettings.h"
 
+#include <BYardSDK/ASteer.h>
 #include <BYardSDK/AGUI2.h>
 
 //-----------------------------------------------------------------------------
@@ -60,55 +61,6 @@ struct AUnitPlayer
 };
 
 struct APlayerManager;
-struct ASteer : public TObject
-{
-	virtual void Unknown1();
-	virtual void Unknown2();
-	virtual void Unknown3();
-	virtual void Unknown4();
-	virtual void Unknown5();
-	virtual void Unknown6();
-	virtual void Unknown7();
-	virtual void Unknown8();
-	virtual void Unknown9();
-	virtual void Unknown10();
-	virtual void Unknown11();
-	virtual void Unknown12();
-	virtual void Unknown13();
-	virtual void Unknown14();
-	virtual void Unknown15();
-	virtual void Unknown16();
-	virtual void Unknown17();
-	virtual void Unknown18();
-	virtual void Unknown19();
-	virtual void Unknown20();
-	virtual void Unknown21();
-	virtual void Unknown22();
-	virtual void Unknown23();
-	virtual void Unknown24();
-	virtual void Unknown25();
-	virtual void Unknown26();
-	virtual void Unknown27();
-	virtual void Unknown28();
-	virtual void Unknown29();
-	virtual void Unknown30();
-	virtual void Unknown31();
-	virtual void Unknown32();
-	virtual void Unknown33();
-	virtual void Unknown34();
-	virtual void Unknown35();
-	virtual void Unknown36();
-	virtual void Unknown37();
-	virtual void Unknown38();
-	virtual void Unknown39();
-	virtual void Unknown40();
-	virtual void Unknown41();
-	virtual void Unknown42();
-	virtual void Unknown43();
-	virtual void Unknown44();
-	virtual void Unknown45();
-	virtual void GetVelocityVector( TVector4& a_rOutVector );
-};
 
 void AGUISpeedometer::Update()
 {
---
+++ SDK/BYardSDK/Source/BYardSDK/ASteer.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <Toshi/TObject.h>
+#include <Math/TVector4.h>
+
+struct ASteer
+    : public Toshi::TObject
+{
+	virtual void            Unknown1();
+	virtual void            Unknown2();
+	virtual void            Unknown3();
+	virtual void            Unknown4();
+	virtual void            Unknown5();
+	virtual void            Unknown6();
+	virtual void            Unknown7();
+	virtual void            Unknown8();
+	virtual void            Unknown9();
+	virtual void            Unknown10();
+	virtual void            Unknown11();
+	virtual void            Unknown12();
+	virtual void            Unknown13();
+	virtual void            Unknown14();
+	virtual void            Unknown15();
+	virtual void            Unknown16();
+	virtual void            Unknown17();
+	virtual void            Unknown18();
+	virtual void            Unknown19();
+	virtual void            Unknown20();
+	virtual void            Unknown21();
+	virtual void            Unknown22();
+	virtual void            Unknown23();
+	virtual void            Unknown24();
+	virtual void            Unknown25();
+	virtual void            Unknown26();
+	virtual void            Unknown27();
+	virtual void            Unknown28();
+	virtual void            Unknown29();
+	virtual void            Unknown30();
+	virtual void            Unknown31();
+	virtual void            Unknown32();
+	virtual void            Unknown33();
+	virtual void            Unknown34();
+	virtual void            Unknown35();
+	virtual void            Unknown36();
+	virtual void            Unknown37();
+	virtual void            Unknown38();
+	virtual void            Unknown39();
+	virtual void            Unknown40();
+	virtual void            Unknown41();
+	virtual void            Unknown42();
+	virtual Toshi::TVector4 GetPosition();
+	virtual void            Unknown44();
+	virtual void            Unknown45();
+	virtual void            GetVelocityVector( Toshi::TVector4& a_rOutVector );
+};
