d15778b130ff2da4007d3dfe5455c49763d21445
InfiniteC0re <inf.core0@gmail.com>
2025-09-24 16:09:34 +0500

    [ModCore] Add -noimgui start parameter

--- SDK/BYModCore/Source/AHooks.cpp
+++ SDK/BYModCore/Source/AHooks.cpp
@@ -564,7 +564,7 @@ MEMBER_HOOK( 0x0060c7c0, ARenderer, ARenderer_OnCreate, TBOOL )
 	if ( g_bUsingDXVK )
 		TINFO( "DXVK transition layer detected\n" );
 	
-	if (false && !g_pCommandLine->HasParameter("-noimgui"))
+	if (!g_pCommandLine->HasParameter("-noimgui"))
 		AImGUI::CreateSingleton();
 
 	return bResult;
